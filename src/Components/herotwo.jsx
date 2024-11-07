1ba2118f8805c5a2330abf22e63498f345018ca0
holluville <femolola@gmail.com>
2024-11-07 16:24:12 +0100

    added

--- src/Components/herotwo.jsx
+++ src/Components/herotwo.jsx
@@ -5,21 +5,29 @@ import "./Herotwo.css"
 // import { FaBalanceScale } from "react-icons/fa";
 // import { FaLocationArrow } from "react-icons/fa";
 // import { FaPiggyBank } from "react-icons/fa";
-import Page2 from "../assets/page2.png"
+// import Page2 from "../assets/page2.png"
+
+// const Herotwo = () => {
+//     return (  
+//         <div className="pan">
+//             <img src={Page2} alt="p"/>
+
+//             {/* <Propsthree Image={IoCard} Comment='Order a Kuda card on the app with pickup and delivery options.'/>
+//             <Propsthree Image={RiPieChart2Fill} Comment='Enjoy cashless payment options online and offline.'/>
+//             <Propsthree Image={FaBalanceScale} Comment='Pay your essential bills and buy gift cards easily'/>
+//             <Propsthree Image={FaLocationArrow} Comment='Get 25 free transfers to Nigerian banks every month.'/>
+//             <Propsthree Image={FaPiggyBank} Comment='Save money automatically any time you spend.'/>
+//    */}
+//         </div>
+//     );
+// }
+ 
+// export default Herotwo;
 
 const Herotwo = () => {
-    return (  
-        <div className="pan">
-            <img src={Page2} alt="p"/>
+    return ( <div>
 
-            {/* <Propsthree Image={IoCard} Comment='Order a Kuda card on the app with pickup and delivery options.'/>
-            <Propsthree Image={RiPieChart2Fill} Comment='Enjoy cashless payment options online and offline.'/>
-            <Propsthree Image={FaBalanceScale} Comment='Pay your essential bills and buy gift cards easily'/>
-            <Propsthree Image={FaLocationArrow} Comment='Get 25 free transfers to Nigerian banks every month.'/>
-            <Propsthree Image={FaPiggyBank} Comment='Save money automatically any time you spend.'/>
-   */}
-        </div>
-    );
+    </div> );
 }
  
 export default Herotwo;
